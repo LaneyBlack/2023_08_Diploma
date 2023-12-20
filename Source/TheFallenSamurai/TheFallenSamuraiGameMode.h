dd9f383c3f604e6178e7d717a91122277e1220f7
Laney Black PC <laney.black.74@gmail.com>
2023-12-20 14:10:06 +0100

    Project setted up.

---
+++ Source/TheFallenSamurai/TheFallenSamuraiGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheFallenSamuraiGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATheFallenSamuraiGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATheFallenSamuraiGameMode();
+};
+
+
+
