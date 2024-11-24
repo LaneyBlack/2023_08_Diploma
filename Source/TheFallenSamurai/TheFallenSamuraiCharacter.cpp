f937846099920de4e3b21226b21bc4fc660b8f10
Laney Black PC <laney.black.74@gmail.com>
2024-11-24 22:18:35 +0100

    Merge fixes

--- Source/TheFallenSamurai/TheFallenSamuraiCharacter.cpp
+++ Source/TheFallenSamurai/TheFallenSamuraiCharacter.cpp
@@ -381,8 +381,8 @@ void ATheFallenSamuraiCharacter::Look(const FInputActionValue& Value)
 	if (Controller != nullptr)
 	{
 		// add yaw and pitch input to controller
-		//AddControllerYawInput(LookAxisVector.X);
-		//AddControllerPitchInput(LookAxisVector.Y);
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
 
 		DesiredCharacterYaw = Cast<APlayerController>(Controller)->RotationInput.Yaw;
 	}
